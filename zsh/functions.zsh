abb8541d1d77f1770b010ac07546a796c9a4e2f0
David Johansson <david.u.johansson@gmail.com>
2020-02-14 13:47:18 +0100

    Fix fzf function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -60,3 +60,19 @@ function unidecode() {
     perl -e "binmode(STDOUT, ':utf8'); print \"$@\""
     echo # newline
 }
+
+fag(){
+  local line
+  line=`ag --nocolor "$1" | fzf` \
+    && vim $(cut -d':' -f1 <<< "$line") +$(cut -d':' -f2 <<< "$line")
+}
+
+# alternative using ripgrep-all (rga) combined with fzf-tmux preview
+# implementation below makes use of "open" on macOS, which can be replaced by other commands if needed.
+# allows to search in PDFs, E-Books, Office documents, zip, tar.gz, etc. (see https://github.com/phiresky/ripgrep-all)
+# find-in-file - usage: fif <searchTerm> or fif "string with spaces" or fif "regex"
+fif() {
+    if [ ! "$#" -gt 0 ]; then echo "Need a string to search for!"; return 1; fi
+    local file
+    file="$(rga --max-count=1 --ignore-case --files-with-matches --no-messages "$@" | fzf-tmux +m --preview="rga --ignore-case --pretty --context 10 '"$@"' {}")" && nvim "$file"
+}
